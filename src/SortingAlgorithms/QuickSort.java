d08dd645e843442eaf9cce6d4a458b387ec2efc7
Divya Jain <divya.jain@veritas.com>
2019-02-22 22:25:32 +0530

    Quick sort

--- src/SortingAlgorithms/QuickSort.java
+++ src/SortingAlgorithms/QuickSort.java
@@ -66,3 +66,41 @@ public class QuickSort {
         sort(arr, 0, arr.length - 1);
     }
 }
+
+/**
+ output:
+ startIndex: 0, endIndex: 5
+ current array  -> 10 -> 7 -> 8 -> 9 -> 1 -> 5
+ pivot : 5, at position: 5
+ low: 0, high: 4
+ calling swap , array after swap as below:
+ current array  -> 1 -> 7 -> 8 -> 9 -> 10 -> 5
+ low: 1, high: 0
+ Array after pivot element swap with low at: 1 and pivot at: 5
+ current array  -> 1 -> 5 -> 8 -> 9 -> 10 -> 7
+ total swaps done: 1, returning: 1
+
+ startIndex: 2, endIndex: 5
+ current array  -> 1 -> 5 -> 8 -> 9 -> 10 -> 7
+ pivot : 7, at position: 5
+ low: 2, high: 1
+ Array after pivot element swap with low at: 2 and pivot at: 5
+ current array  -> 1 -> 5 -> 7 -> 9 -> 10 -> 8
+ total swaps done: 0, returning: 2
+
+ startIndex: 3, endIndex: 5
+ current array  -> 1 -> 5 -> 7 -> 9 -> 10 -> 8
+ pivot : 8, at position: 5
+ low: 3, high: 2
+ Array after pivot element swap with low at: 3 and pivot at: 5
+ current array  -> 1 -> 5 -> 7 -> 8 -> 10 -> 9
+ total swaps done: 0, returning: 3
+
+ startIndex: 4, endIndex: 5
+ current array  -> 1 -> 5 -> 7 -> 8 -> 10 -> 9
+ pivot : 9, at position: 5
+ low: 4, high: 3
+ Array after pivot element swap with low at: 4 and pivot at: 5
+ current array  -> 1 -> 5 -> 7 -> 8 -> 9 -> 10
+ total swaps done: 0, returning: 4
+ */
