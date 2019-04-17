eda444b6723e1a36dd1a3004d93330adc7bb7053
Divya Jain <divya.jain@veritas.com>
2019-04-17 14:52:47 +0530

    REPO MADE PUBLIC

--- src/Competitive_programming/ChocolateArrangement.java
+++ src/Competitive_programming/ChocolateArrangement.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class RemoveChocolates {
+public class ChocolateArrangement {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
